05ef6517d32aa0ba9ef36e05e1abbe703376b46f
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2017-06-24 08:08:51 +0800

    add express

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000))
+
+app.use('/', express.static("build"));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
