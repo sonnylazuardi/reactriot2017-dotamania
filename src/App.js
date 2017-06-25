03ef0486271ee56f35e4190e12550c7c7a6f344d
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2017-06-26 05:21:42 +0800

    Add on boarding

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SplitterLayout from 'react-splitter-layout';
 import JSONTree from 'react-json-tree'
 import axios from 'axios';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import Slider from 'react-slick';
 
 class App extends Component {
   state = {
@@ -14,6 +15,7 @@ class App extends Component {
     result: null,
     copied: false,
     childType: 'node',
+    onboarding: true,
   }
   componentWillReceiveProps(nextProps) {
     if (this.props.activeNode != nextProps.activeNode) {
@@ -143,9 +145,23 @@ class App extends Component {
       selector: this.props.activeNode.selector,
     });
   }
+  onStart = () => {
+    this.setState({
+      onboarding: false,
+    });
+  }
   render() {
     const {activeNode, editable} = this.props;
-    const {title, selector, result, isAddChild} = this.state;
+    const {title, selector, result, isAddChild, onboarding} = this.state;
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+    };
+
     return (
       <div style={styles.container}>
         <SplitterLayout>
@@ -242,6 +258,29 @@ class App extends Component {
             </SplitterLayout>
           </div>
         </SplitterLayout>
+        {onboarding ?
+          <div style={styles.onboarding}>
+            <div style={styles.white}></div>
+            <div style={styles.screenshot}></div>
+            <div style={styles.sliderWrapper}>
+              <Slider {...settings}>
+                <div>
+                  <h3>Mind Map</h3>
+                  <p>Arrange the things that you want to scrape visually using mind map</p>
+                </div>
+                <div>
+                  <h3>Node Editor</h3>
+                  <p>Edit the title and selector of the node easily within the web</p>
+                </div>
+                <div>
+                  <h3>Export To JSON</h3>
+                  <p>Easily browse and export the scraping result to JSON</p>
+                </div>
+              </Slider>
+            </div>
+            <a href="#" style={styles.startButton} onClick={this.onStart}>🏃 START</a>
+          </div>
+          : null}
       </div>
     );
   }
@@ -376,6 +415,53 @@ const styles = {
     margin: '0 5px 10px 0',
     fontSize: '12px',
     textDecoration: 'none',
+  },
+  onboarding: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(75,144,247,.8)',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    flexDirection: 'column',
+  },
+  screenshot: {
+    background: 'url("/images/screenshot.png") no-repeat',
+    width: 400,
+    height: 300,
+    margin: '20px',
+    backgroundSize: 'contain',
+    backgroundPosition: 'center',
+    zIndex: 1,
+  },
+  white: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 400,
+    backgroundColor: 'rgba(255,255,255,.9)',
+    zIndex: 0,
+  },
+  sliderWrapper: {
+    width: 320,
+  },
+  startButton: {
+    backgroundColor: '#4b90f7',
+    padding: '20px 60px',
+    width: 100,
+    height: 25,
+    borderRadius: 50,
+    textAlign: 'center',
+    fontWeight: '900',
+    textDecoration: 'none',
+    color: '#fff',
+    zIndex: 2,
   },
 };
 
